0f64d375ec9306fbe20259b2ab85feb2fa698b8c
FelixCLC (FCLC) <felix.leclair123@hotmail.com>
2022-04-21 14:01:54 -0400

    swap hard codded threads for $(nproc)

--- bench_4k.sh
+++ bench_4k.sh
@@ -3,22 +3,22 @@
 echo "bench_4k.sh"
 echo ""
 echo "base"
-dav1d -i source_4k_sdr.mkv --muxer null --threads 16 --filmgrain 0 --cpumask 0
+dav1d -i source_4k_sdr.mkv --muxer null --threads $(nproc) --filmgrain 0 --cpumask 0
 echo ""
 echo "sse2"
-dav1d -i source_4k_sdr.mkv --muxer null --threads 16 --filmgrain 0 --cpumask sse2
+dav1d -i source_4k_sdr.mkv --muxer null --threads $(nproc) --filmgrain 0 --cpumask sse2
 echo ""
 echo "sse41"
-dav1d -i source_4k_sdr.mkv --muxer null --threads 16 --filmgrain 0 --cpumask sse41
+dav1d -i source_4k_sdr.mkv --muxer null --threads $(nproc) --filmgrain 0 --cpumask sse41
 echo ""
 echo "avx2"
-dav1d -i source_4k_sdr.mkv --muxer null --threads 16 --filmgrain 0 --cpumask avx2
+dav1d -i source_4k_sdr.mkv --muxer null --threads $(nproc) --filmgrain 0 --cpumask avx2
 echo ""
 echo "avx512icl"
-dav1d -i source_4k_sdr.mkv --muxer null --threads 16 --filmgrain 0 --cpumask avx512icl
+dav1d -i source_4k_sdr.mkv --muxer null --threads $(nproc) --filmgrain 0 --cpumask avx512icl
 echo ""
 echo "All available"
-dav1d -i source_4k_sdr.mkv --muxer null --threads 16 --filmgrain 0 --cpumask -1
+dav1d -i source_4k_sdr.mkv --muxer null --threads $(nproc) --filmgrain 0 --cpumask -1
 echo ""
 
 
